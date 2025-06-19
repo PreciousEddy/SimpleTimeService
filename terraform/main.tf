f8a19f6b76888a140b3d32679a7b78180de544ee
Precious Edmund <edmundprecious23@gmail.com>
2025-06-19 19:56:58 +0000

    Updated the main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -74,13 +74,6 @@ resource "google_api_gateway_gateway" "gateway" {
   location   = var.region
 }
 
-output "cloud_run_url" {
-  value = google_cloud_run_service.default.status[0].url
-}
-
-output "api_gateway_url" {
-  value = "https://${google_api_gateway_gateway.gateway.default_hostname}"
-}
 
 
 
