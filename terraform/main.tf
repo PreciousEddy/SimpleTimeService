eb5c0b29dcacf73036c773b2c5aba703fc794502
Precious Edmund <edmundprecious23@gmail.com>
2025-06-20 00:09:16 +0100

    Updated ECS task Definition to use ip

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,25 +2,29 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_availability_zones" "available" {}
+
+# VPC
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
   enable_dns_hostnames = true
 }
 
+# Subnets
 resource "aws_subnet" "public" {
-  count             = 2
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index)
+  count                   = 2
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet(aws_vpc.main.cidr_block, 4, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
   map_public_ip_on_launch = true
-  availability_zone = data.aws_availability_zones.available.names[count.index]
 }
 
-data "aws_availability_zones" "available" {}
-
+# Internet Gateway
 resource "aws_internet_gateway" "gw" {
   vpc_id = aws_vpc.main.id
 }
 
+# Route Table
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
 
@@ -36,6 +40,7 @@ resource "aws_route_table_association" "a" {
   route_table_id = aws_route_table.public.id
 }
 
+# Security Group
 resource "aws_security_group" "ecs_sg" {
   vpc_id = aws_vpc.main.id
 
@@ -54,10 +59,12 @@ resource "aws_security_group" "ecs_sg" {
   }
 }
 
+# ECS Cluster
 resource "aws_ecs_cluster" "main" {
   name = var.service_name
 }
 
+# Task Definition
 resource "aws_ecs_task_definition" "app" {
   family                   = var.service_name
   requires_compatibilities = ["FARGATE"]
@@ -65,13 +72,21 @@ resource "aws_ecs_task_definition" "app" {
   memory                   = "512"
   network_mode             = "awsvpc"
 
-  container_definitions = jsonencode([{
-    name      = var.service_name
-    image     = var.docker_image
-    portMappings = [{ containerPort = 5000 }]
-  }])
+  container_definitions = jsonencode([
+    {
+      name  = var.service_name
+      image = var.docker_image
+      portMappings = [
+        {
+          containerPort = 5000
+          protocol      = "tcp"
+        }
+      ]
+    }
+  ])
 }
 
+# Load Balancer
 resource "aws_lb" "main" {
   name               = "${var.service_name}-lb"
   internal           = false
@@ -80,25 +95,26 @@ resource "aws_lb" "main" {
   security_groups    = [aws_security_group.ecs_sg.id]
 }
 
+# Target Group
 resource "aws_lb_target_group" "tg" {
-  name     = "${var.service_name}-tg"
-  port     = 5000
-  protocol = "HTTP"
-  vpc_id   = aws_vpc.main.id
+  name        = "${var.service_name}-tg"
+  port        = 5000
+  protocol    = "HTTP"
+  vpc_id      = aws_vpc.main.id
   target_type = "ip"
 
   health_check {
-    enabled = true
-    path = "/"
-    protocol = "HTTP"
-    port = "5000"
-    healthy_threshold = 3
+    enabled             = true
+    path                = "/"
+    port                = "5000"
+    protocol            = "HTTP"
+    healthy_threshold   = 3
     unhealthy_threshold = 3
-    interval = 30
+    interval            = 30
   }
-  depends_on = [aws_lb_listener.listener]
 }
 
+# Listener
 resource "aws_lb_listener" "listener" {
   load_balancer_arn = aws_lb.main.arn
   port              = 80
@@ -110,13 +126,13 @@ resource "aws_lb_listener" "listener" {
   }
 }
 
+# ECS Service
 resource "aws_ecs_service" "app" {
   name            = var.service_name
   cluster         = aws_ecs_cluster.main.id
   task_definition = aws_ecs_task_definition.app.arn
   launch_type     = "FARGATE"
-
-  desired_count = 1
+  desired_count   = 1
 
   network_configuration {
     subnets         = aws_subnet.public[*].id
